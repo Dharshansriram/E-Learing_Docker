37fed9b96de64aebc1ed9ea1e0fc93946b07bc4d
Dharshansriram <dharshansriram.r@gmail.com>
2026-03-01 15:03:59 +0530

    Add files via upload

---
+++ E-Learning_Server/E-Learning_Frontend/Front-JS/JS-utils/sessionEngine.js
@@ -0,0 +1,40 @@
+function startAssessmentSession(skill, mode) {
+    let questions = [];
+    const page = document.getElementById("codearea");
+
+    if (skill === "aptitude") {
+        questions = getAptitudeQuestions(mode);
+    }
+
+    if (skill === "dsa") {
+        questions = getDsaQuestions(mode);
+    }
+
+    if (mode === "codedsa") {
+        page.classList.remove("hidden");
+    }
+
+
+
+    AppState.session = {
+        questions: shuffleArray(questions).slice(0, 50),
+        index: 0
+    };
+
+    initScoreEngine();
+}
+
+function getCurrentQuestion() {
+    return AppState.session.questions[AppState.session.index];
+}
+
+function nextQuestion() {
+    AppState.session.index++;
+
+    if (AppState.session.index >= AppState.session.questions.length) {
+        stopTimer && stopTimer();
+        finishAssessment();
+    } else {
+        navigate("assessment");
+    }
+}
